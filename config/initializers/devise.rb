c7217b7f8caf39ac5fc962d172c0527d8300fd2f
Pavel Dotsulenko <paul@live.ru>
2014-02-11 20:35:56 +0200

    .travis..yml fixed

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -2,7 +2,7 @@
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
 
-  AppConfig = YAML.load_file('../config/config.yml')
+  AppConfig = YAML.load_file('config/config.yml')
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
