9d4768ef386f1eb84c13968f69bb24317c54c47a
Pavel Dotsulenko <paul@live.ru>
2014-02-15 18:12:48 +0200

    Errors controller implemented

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,4 +33,6 @@ Ecomm::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.exceptions_app = self.routes  
 end
