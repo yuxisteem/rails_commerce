d360f83e8de8be3988ac56e5c4711fbc4f106cc1
Pavel Dotsulenko <paul@live.ru>
2014-02-18 00:10:31 +0200

    Puma config updated

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,6 +10,8 @@ set :deploy_to, '/home/rails'
 set :deploy_via, :remote_cache
 set :copy_exclude, [ '.git' ]
 
+set :rails_env, 'production'
+
 set :keep_releases, 2
 
 set :current_release, "#{fetch(:deploy_to)}/current"
@@ -18,12 +20,12 @@ set :rvm_ruby_string, 'ruby-2.1.0'
 
 set :rvm_bin_path, '/home/rails/.rvm/bin/rvm'
 
-#set :linked_dirs, %w{bin log vendor/bundle public/system}
-#set :linked_files, %w(config/config.yml config/database.yml config/newrelic.yml)
+set :linked_dirs, %w{bin log vendor/bundle public/system}
+set :linked_files, %w(config/config.yml config/database.yml config/newrelic.yml)
 
 set :puma_pid_file, "#{fetch(:deploy_to)}/shared/tmp/pids/puma.pid"
 set :puma_state_file, "#{fetch(:deploy_to)}/shared/tmp/pids/puma.state"
-set :config_file, "#{fetch(:current_release)}/config/puma.rb"
+set :config_file, "#{release_path}/config/puma.rb"
 
 set :puma_cmd, "puma -C #{fetch(:config_file)}"
 
@@ -32,15 +34,15 @@ def bundler
 end
 
 def puma_start_cmd
-  "cd #{fetch(:current_release)} && #{bundler} pumactl -F #{fetch(:config_file)} -S #{fetch(:puma_state_file)} start"
+  "cd #{release_path} && #{bundler} pumactl -F #{fetch(:config_file)} -S #{fetch(:puma_state_file)} start"
 end
 
 def puma_stop_cmd
-  "cd #{fetch(:current_release)} && #{bundler} pumactl -S #{fetch(:puma_state_file)} stop"
+  "cd #{release_path} && #{bundler} pumactl -S #{fetch(:puma_state_file)} stop"
 end
 
 def puma_restart_cmd
-  "cd #{fetch(:current_release)} && #{bundler} pumactl -F #{fetch(:config_file)} -S #{fetch(:puma_state_file)} restart"
+  "cd #{release_path} && #{bundler} pumactl -F #{fetch(:config_file)} -S #{fetch(:puma_state_file)} restart"
 end
 
 def linked_dirs
@@ -53,17 +55,17 @@ end
 
 namespace :deploy do
 
-  task :start do
-    on roles(:app), in: :sequence, wait: 5 do
-      execute "#{puma_start_cmd}", :pty => false
-    end
-  end
+  # task :start do
+  #   on roles(:app), in: :sequence, wait: 5 do
+  #     execute "#{puma_start_cmd}", :pty => false
+  #   end
+  # end
 
-  task :stop do
-    on roles(:app), in: :sequence, wait: 5 do
-      execute "#{puma_stop_cmd}"
-    end
-  end
+  # task :stop do
+  #   on roles(:app), in: :sequence, wait: 5 do
+  #     execute "#{puma_stop_cmd}"
+  #   end
+  # end
 
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
@@ -71,7 +73,6 @@ namespace :deploy do
         execute "#{puma_restart_cmd}"            
       rescue Exception => ex 
         puts "Failed to restart puma: #{ex}\nAssuming not started."
-        execute "#{puma_start_cmd}"
       end
     end
   end
