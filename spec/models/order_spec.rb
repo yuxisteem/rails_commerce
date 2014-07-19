4ef92f693fc7c1b2f06e8e2ad57d338427c885a5
Pavel Dotsulenko <paul@live.ru>
2014-07-19 15:34:42 +0300

    Remove old test. Replace tabs to spaces

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -15,42 +15,34 @@
 require 'spec_helper'
 
 describe Order do
-  let(:order) {create(:order)}
+  let(:order) { create(:order) }
 
-  it "should create order from cart" do
-  	cart = create(:cart)
-
-  	order = Order.build_from_cart(cart)
-  	order.update(user: create(:user), address: create(:address))
-  	expect(order.order_items.count).to eq(cart.cart_items.count)
+  it 'should have invoice generated automatically' do
+    order.invoice.should be_true
   end
 
-  it "should have invoice generated automatically" do
-  	order.invoice.should be_true
+  it 'should have shipment generated automatically' do
+    order.shipment.should be_true
   end
 
-  it "should have shipment generated automatically" do
-  	order.shipment.should be_true
-  end
-
-  describe "state machine" do
-    it "should have initial In Progress state" do
+  describe 'state machine' do
+    it 'should have initial In Progress state' do
       Order.new.aasm.current_state.should eq(:in_progress)
     end
 
-    it "should log state transitions" do
+    it 'should log state transitions' do
       order.cancel!
       order.order_histories.last.to_name.should eq('canceled')
     end
 
-    it "should be in Completed state if invoice is paid and shipment shipped" do
+    it 'should be in Completed state if invoice is paid and shipment shipped' do
       order.invoice.pay!
       order.shipment.prepare!
       order.shipment.ship!
       order.aasm.current_state.should eq(:completed)
     end
 
-    it "should be in In Progress state unless invoice is paid and shipment shipped" do
+    it 'should be in In Progress state unless invoice is paid and shipment shipped' do
       order.invoice.pay!
       order.shipment.prepare!
       order.shipment.ship!
@@ -58,11 +50,11 @@ describe Order do
       order.aasm.current_state.should eq(:in_progress)
     end
 
-    it "should be cancelable if not shipped and not paid" do
+    it 'should be cancelable if not shipped and not paid' do
       order.may_cancel?.should be_true
     end
 
-    it "should not be cancelable if shipped or paid" do
+    it 'should not be cancelable if shipped or paid' do
       order.invoice.pay
       order.may_cancel?.should be_false
     end
