292e161d1f7461e2080635edd84ba2c9a4801dff
Pavel Dotsulenko <paul@live.ru>
2014-07-19 15:42:50 +0300

    Test for Cart updated

--- spec/factories/carts.rb
+++ spec/factories/carts.rb
@@ -10,19 +10,19 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  	factory :cart do
-      # items_count is declared as an ignored attribute and available in
-      # attributes on the factory, as well as the callback via the evaluator
-      ignore do
-        items_count 5
-      end
-
-      # the after(:create) yields two values; the user instance itself and the
-      # evaluator, which stores all values from the factory, including ignored
-      # attributes; `create_list`'s second argument is the number of records
-      # to create and we make sure the cart is associated properly to the cart_item
-      after(:create) do |cart, evaluator|
-        create_list(:cart_item, evaluator.items_count, cart: cart)
-      end
+  factory :cart do
+    # items_count is declared as an ignored attribute and available in
+    # attributes on the factory, as well as the callback via the evaluator
+    ignore do
+      items_count 5
     end
+
+    # the after(:create) yields two values; the user instance itself and the
+    # evaluator, which stores all values from the factory, including ignored
+    # attributes; `create_list`'s second argument is the number of records
+    # to create and we make sure the cart is associated properly to the cart_item
+    after(:create) do |cart, evaluator|
+      create_list(:cart_item, evaluator.items_count, cart: cart)
+    end
+  end
 end
---
+++ spec/models/cart_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe Cart do
+  let(:cart) { create(:cart) }
+  describe '#to_order_items' do
+    it 'should generate order items' do
+      order_items = cart.to_order_items
+
+      expect(order_items.count).to eq(cart.cart_items.count)
+      expect(order_items).to be_an(Array)
+      expect(order_items.first).to be_an(OrderItem)
+    end
+  end
+end
