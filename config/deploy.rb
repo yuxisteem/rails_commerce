809e64e94b785c7d81ec292e8b578ae4ac1fe803
Pavel Dotsulenko <paul@live.ru>
2014-08-03 23:07:36 +0300

    Deploy cuurent branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :rbenv_ruby, '2.1.2'
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
-# set :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call'
+set :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app
 # set :deploy_to, '/var/www/my_app'
