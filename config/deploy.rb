1d4939c1b841ac5d12d87c6820d44f787ca30be8
Pavel Dotsulenko <paveldotsulenko@livenation.com>
2014-08-20 19:28:17 +0300

    Post deploy task updated

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,8 +43,8 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      invoke 'foreman:export'
+      invoke 'foreman:restart'
     end
   end
 
