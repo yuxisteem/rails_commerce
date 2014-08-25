da7f1453f2a4f126b660fafd05728a9a8f5eb2ed
Pavel Dotsulenko <paul@live.ru>
2014-08-25 14:39:15 +0300

    Fix email address

--- app/mailers/order_notifier.rb
+++ app/mailers/order_notifier.rb
@@ -8,7 +8,7 @@ class OrderNotifier < AsyncMailer
   # Notify Admin that we've recieved new order
   def order_received_admin(order_id, user_id)
     @order = Order.find(order_id)
-    mail to: User.find(user_id)
+    mail to: User.find(user_id).email
   end
 
   # Notify Customer that we've shipped order
