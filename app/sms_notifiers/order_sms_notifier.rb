6ac93ace870919b148fdc48c8f81675df33a7b5c
Pavel Dotsulenko <paul@live.ru>
2014-08-30 02:34:45 +0300

    Get phone number from address

--- app/sms_notifiers/order_sms_notifier.rb
+++ app/sms_notifiers/order_sms_notifier.rb
@@ -1,7 +1,7 @@
 class OrderSMSNotifier < SMSNotifier
   def order_received(order_id)
     @order = Order.find(order_id)
-    sms "#{@order.user.full_name}, cпасибо за ваш заказ №#{@order.id}. Мы свяжемся с вами в ближайшее время. :)", to: @order.user.phone
+    sms "#{@order.user.full_name}, cпасибо за ваш заказ №#{@order.id}. Мы свяжемся с вами в ближайшее время. :)", to: @order.address.phone
   end
 
   def order_received_admin(order_id)
