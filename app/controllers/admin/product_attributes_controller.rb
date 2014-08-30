dadb162be529fdac4b00979ab9e2c0e38766ab8d
Pavel Dotsulenko <paul@live.ru>
2014-08-31 01:56:35 +0300

    Product attributes

--- app/controllers/admin/product_attributes_controller.rb
+++ app/controllers/admin/product_attributes_controller.rb
@@ -2,7 +2,7 @@ class Admin::ProductAttributesController < Admin::AdminController
   def create
     @product_attribute = ProductAttribute.new(product_attributes_params)
     if @product_attribute.save
-      flash[:notice] = I18n.t('admin.product_attribute_saved')
+      flash[:notice] = I18n.t('admin.product_attributes.product_attribute_saved')
     end
     redirect_to admin_category_path(id: params[:category_id])
   end
@@ -10,14 +10,14 @@ class Admin::ProductAttributesController < Admin::AdminController
   def update
     @product_attribute = ProductAttribute.find(params[:id]).update!(product_attributes_params)
     if @product_attribute.save
-      flash[:notice] = I18n.t('admin.product_attribute_updated')
+      flash[:notice] = I18n.t('admin.product_attributes.product_attribute_updated')
     end
     redirect_to admin_category_path(id: params[:category_id])
   end
 
   def destroy
     if ProductAttribute.delete(params[:id])
-      flash[:notice] = I18n.t('admin.product_attribute_deleted')
+      flash[:notice] = I18n.t('admin.product_attributes.product_attribute_deleted')
     end
     redirect_to admin_category_path(id: params[:category_id])
   end
