5704fe525ca025c3c6794d748b10403d5065d012
Pavel Dotsulenko <n.dreadlock@gmail.com>
2014-11-15 19:20:46 +0200

    Fix x_editable helper

--- app/helpers/admin/x_editable_helper.rb
+++ app/helpers/admin/x_editable_helper.rb
@@ -1,6 +1,6 @@
 module Admin::XEditableHelper
-  def x_editable_for(model, path, type: 'text', field: nil)
+  def x_editable_for(model, url, type: 'text', field: nil)
     model_name = ActiveModel::Naming.singular(model)
-    link_to model.send(field) || '', '#', data: { name: model_name, type: type, path: path, xeditable: true }
+    link_to model.send(field) || '', '#', data: { name: model_name, type: type, url: url, xeditable: true }
   end
 end
